96418492194a06ed3f3cb4b08c68d635e9e48ee2
Ron <Rayron5656@Gmail.com>
2022-04-05 19:46:52 +0300

    k

--- src/app/dogs/dogs.component.ts
+++ src/app/dogs/dogs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MyHTTPService } from '../my-http.service';
 
 @Component({
   selector: 'app-dogs',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DogsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _myHttp:MyHTTPService) { }
+
+  dogPic:string = '';
 
   ngOnInit(): void {
+  }
+
+  getDogImg(){
+    this._myHttp.getDogs().subscribe((dog) => console.log(dog));
   }
 
 }
